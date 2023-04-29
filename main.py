2f371d3a544ea85d83a4f9f383eca1158272650a
Rahul Pillai <94741905+CoderRahu1@users.noreply.github.com>
2023-04-29 21:54:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# Modules which need to be imported
+import webbrowser
+
+# Site which you want to Open in your
+# Browser
+url = "mail.google.com"
+
+# Below code is used to specify
+# location of webbrowser which you
+# want to use.
+edge_path = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+
+# Below code will open your URL
+webbrowser.register(
+'firefox', None, webbrowser.BackgroundBrowser(edge_path))
+
+webbrowser.get('firefox').open_new_tab(url)
