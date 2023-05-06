15924f192a7ff31b2d9a3f834585b9b8b1423e5e
Rahul Pillai <94741905+CoderRahu1@users.noreply.github.com>
2023-05-06 22:42:44 +0530

    Update D1_BAND_NAME_GENERATOR.py

--- PYTHON_D1_D50/D1_BAND_NAME_GENERATOR.py
+++ PYTHON_D1_D50/D1_BAND_NAME_GENERATOR.py
@@ -1,3 +1,7 @@
+########3### DAY 1 ####################
+
+#### BAND NAME GENERATOR
+
 print( "Welcome to the Band Name Generator. ") 
 street = input("What's name of the city you grew up in?\n") 
 pet = input("What's your pet's name?\n") 
